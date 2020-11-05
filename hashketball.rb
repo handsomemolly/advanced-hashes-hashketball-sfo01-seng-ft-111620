a7a03b29b853a5a75de4722f437410a763281f9d
Ryan Kendig <ryan.kendig@gmail.com>
2020-11-05 23:40:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -203,7 +203,7 @@ def big_shoe_rebounds
   rebounds = 0
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
-      if player[:shoe] > big_shoe
+      if player[:shoe] > biggest_shoe
         biggest_shoe = player[:shoe]
         rebounds = player[:rebounds]
       end
