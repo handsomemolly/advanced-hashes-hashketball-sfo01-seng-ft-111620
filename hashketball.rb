1335bc93297d94cae9a9e56cea4741921a0a46a4
Ryan Kendig <ryan.kendig@gmail.com>
2020-11-05 22:38:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,8 +129,8 @@ end
 # Write code here
 def num_points_scored(player_name)
   
-  game_hash.values.each do |team|
-    team[:players].each do |player|
+  game_hash.values.each do |team_data|
+    team_data[:players].each do |player|
       return player[:points] if player.has_value?(player_name)
     end
   end
